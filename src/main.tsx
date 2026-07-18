3a32553fbffb55e0671f80c7fa2215fd5d5f6375
Valeriy Tsvigun <valeriytsvigun@MacBook-Air-Valeriy.local>
2026-07-18 22:04:34 +0500

    Add self-hosted font, design tokens, and CSS reset

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './styles/reset.css'
+import './styles/fonts.css'
+import './styles/tokens.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
